edb0233e8c9073b1611def04e18851a0652941ea
cawauchi6204 <cawauchi6204@gmail.com>
2022-08-29 15:40:22 +0900

    I: 配列型

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hoge")
+	var arr1 [3]int
+	// [3]int 全てが型
+	fmt.Println(arr1)
+	fmt.Printf("%T\n", arr1)
+
+	var arr2 [3]string = [3]string{"A", "B"}
+	fmt.Println(arr2)
+	// 入力されていなければから文字が代入される
+
+	arr3 := [3]int{1, 2, 3}
+	fmt.Println(arr3)
+
+	arr4 := [...]string{"C", "D"}
+	fmt.Println(arr4)
+
+	fmt.Println(arr1[0])
 }
