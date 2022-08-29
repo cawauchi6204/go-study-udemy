2e547251e11bd0464b8886cbba3d191287cd74de
cawauchi6204 <cawauchi6204@gmail.com>
2022-08-29 16:11:40 +0900

    I: スライス

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	var s string = "100"
+	sl := []int{1, 2}
+	fmt.Println(sl)
 
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(i)
+	sl4 := make([]int, 5)
+	// makeは初期値を入れてくれる
+	fmt.Println(sl4)
+
+	sl4[1] = 1000
+	fmt.Println(sl4)
+
+	sl5 := []int{1, 2, 3, 4, 5}
+	fmt.Println(sl5)
+
+	fmt.Println(sl5[2:4])
 }
